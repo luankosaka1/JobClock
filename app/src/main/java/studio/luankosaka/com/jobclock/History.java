bc5a75d23bd9313c25567c162b91109b91feca7d
Luan Kosaka <luank@ipset.com.br>
2014-10-21 18:25:41 -0200

    History

--- app/src/main/java/studio/luankosaka/com/jobclock/History.java
+++ app/src/main/java/studio/luankosaka/com/jobclock/History.java
@@ -1,17 +1,27 @@
 package studio.luankosaka.com.jobclock;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import studio.luankosaka.com.jobclock.sql.DatabaseHelper;
 
 
 public class History extends Activity {
+    DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +29,35 @@ public class History extends Activity {
         setContentView(R.layout.activity_history);
 
         ListView listView = (ListView) findViewById(R.id.listView);
-        String[] values = new String[] {"Android", "iPhone", "WindowsMobile"};
-
-        ArrayList<String> list = new ArrayList<String>();
-        for (int i=0; i < values.length; ++i) {
-            list.add(values[i]);
-            Toast.makeText(this, values[i], Toast.LENGTH_SHORT).show();
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+        listView.setAdapter(getHistory());
     }
 
+    private ArrayAdapter getHistory() {
+        Cursor cursor = selectHistory();
+        cursor.moveToFirst();
+
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i=0; i < cursor.getCount(); ++i) {
+            Integer user_id = cursor.getInt(1);
+            String time_point = cursor.getString(2);
+            list.add("#" + user_id + " : " + time_point);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        return new ArrayAdapter<String>(this, R.layout.list_item_custom, list);
+    }
+
+    private Cursor selectHistory() {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String now = date.format(new Date());
+        helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        return db.rawQuery(
+                "SELECT _id, user_id, time_point FROM work WHERE user_id = ? AND time_point LIKE '%" + now + "%' ORDER BY time_point DESC",
+                new String[] {"1"}
+        );
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
